c68d7f59874fdd6dce4e665baa522ecaf08687eb
Tatsuya Ishii <tishii2479@gmail.com>
2023-12-03 18:05:16 +0900

    reorder

--- src/solver.rs
+++ src/solver.rs
@@ -5,81 +5,6 @@ use crate::util::*;
 
 type Adj = Vec<Vec<Vec<(Dir, (usize, usize))>>>;
 
-fn create_adj(input: &Input) -> Adj {
-    let mut adj = vec![vec![vec![]; input.n]; input.n];
-    for j in 0..input.n {
-        for i in 0..input.n {
-            // 上
-            if i > 0 && !input.h[i - 1][j] {
-                adj[i][j].push((Dir::Up, (i - 1, j)));
-            }
-            // 右
-            if j < input.n - 1 && !input.v[i][j] {
-                adj[i][j].push((Dir::Right, (i, j + 1)));
-            }
-            // 下
-            if i < input.n - 1 && !input.h[i][j] {
-                adj[i][j].push((Dir::Down, (i + 1, j)));
-            }
-            // 左
-            if j > 0 && !input.v[i][j - 1] {
-                adj[i][j].push((Dir::Left, (i, j - 1)));
-            }
-        }
-    }
-    adj
-}
-
-fn calc_r(input: &Input) -> Vec<Vec<f64>> {
-    let mut a_sum = 0.;
-    let mut r = vec![vec![0.; input.n]; input.n];
-    for i in 0..input.n {
-        for j in 0..input.n {
-            r[i][j] = (input.d[i][j] as f64).powf(1. / 3.);
-            a_sum += r[i][j];
-        }
-    }
-    for i in 0..input.n {
-        for j in 0..input.n {
-            r[i][j] /= a_sum;
-        }
-    }
-    r
-}
-
-fn calc_dist(s: (usize, usize), input: &Input, adj: &Adj) -> Vec<Vec<i64>> {
-    let (s_i, s_j) = s;
-    let mut dist = vec![vec![1 << 30; input.n]; input.n];
-    dist[s_i][s_j] = 0;
-    let mut q = VecDeque::new();
-    q.push_back((s_i, s_j));
-    while let Some((v_i, v_j)) = q.pop_front() {
-        for (_, (nxt_i, nxt_j)) in adj[v_i][v_j].iter() {
-            if dist[*nxt_i][*nxt_j] <= dist[v_i][v_j] + 1 {
-                continue;
-            }
-            dist[*nxt_i][*nxt_j] = dist[v_i][v_j] + 1;
-            q.push_back((*nxt_i, *nxt_j));
-        }
-    }
-    dist
-}
-
-fn get_move_cand(
-    s: (usize, usize),
-    t: (usize, usize),
-    dist: &Vec<Vec<Vec<Vec<i64>>>>,
-    adj: &Adj,
-) -> Vec<Dir> {
-    let mut v = vec![];
-    for (dir, nxt) in adj[s.0][s.1].iter() {
-        if dist[t.0][t.1][s.0][s.1] == dist[t.0][t.1][nxt.0][nxt.1] + 1 {
-            v.push(dir.to_owned());
-        }
-    }
-    v
-}
-
 pub fn solve(input: &Input) -> String {
     const L: usize = 1e4 as usize;
     let r = calc_r(input);
@@ -211,4 +136,79 @@ fn cycles_to_answer(cycles: &Vec<Vec<(usize, usize)>>) -> String {
         ans.push(Dir::from(path[i % path.len()], path[(i + 1) % path.len()]));
     }
     ans.iter().map(|d| d.to_char()).collect()
+}
+
+fn create_adj(input: &Input) -> Adj {
+    let mut adj = vec![vec![vec![]; input.n]; input.n];
+    for j in 0..input.n {
+        for i in 0..input.n {
+            // 上
+            if i > 0 && !input.h[i - 1][j] {
+                adj[i][j].push((Dir::Up, (i - 1, j)));
+            }
+            // 右
+            if j < input.n - 1 && !input.v[i][j] {
+                adj[i][j].push((Dir::Right, (i, j + 1)));
+            }
+            // 下
+            if i < input.n - 1 && !input.h[i][j] {
+                adj[i][j].push((Dir::Down, (i + 1, j)));
+            }
+            // 左
+            if j > 0 && !input.v[i][j - 1] {
+                adj[i][j].push((Dir::Left, (i, j - 1)));
+            }
+        }
+    }
+    adj
+}
+
+fn calc_r(input: &Input) -> Vec<Vec<f64>> {
+    let mut a_sum = 0.;
+    let mut r = vec![vec![0.; input.n]; input.n];
+    for i in 0..input.n {
+        for j in 0..input.n {
+            r[i][j] = (input.d[i][j] as f64).powf(1. / 3.);
+            a_sum += r[i][j];
+        }
+    }
+    for i in 0..input.n {
+        for j in 0..input.n {
+            r[i][j] /= a_sum;
+        }
+    }
+    r
+}
+
+fn calc_dist(s: (usize, usize), input: &Input, adj: &Adj) -> Vec<Vec<i64>> {
+    let (s_i, s_j) = s;
+    let mut dist = vec![vec![1 << 30; input.n]; input.n];
+    dist[s_i][s_j] = 0;
+    let mut q = VecDeque::new();
+    q.push_back((s_i, s_j));
+    while let Some((v_i, v_j)) = q.pop_front() {
+        for (_, (nxt_i, nxt_j)) in adj[v_i][v_j].iter() {
+            if dist[*nxt_i][*nxt_j] <= dist[v_i][v_j] + 1 {
+                continue;
+            }
+            dist[*nxt_i][*nxt_j] = dist[v_i][v_j] + 1;
+            q.push_back((*nxt_i, *nxt_j));
+        }
+    }
+    dist
+}
+
+fn get_move_cand(
+    s: (usize, usize),
+    t: (usize, usize),
+    dist: &Vec<Vec<Vec<Vec<i64>>>>,
+    adj: &Adj,
+) -> Vec<Dir> {
+    let mut v = vec![];
+    for (dir, nxt) in adj[s.0][s.1].iter() {
+        if dist[t.0][t.1][s.0][s.1] == dist[t.0][t.1][nxt.0][nxt.1] + 1 {
+            v.push(dir.to_owned());
+        }
+    }
+    v
 }
